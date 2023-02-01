c32148e51dff07a66330e4c06facbf8fbe655688
Silvana1409 <89832689+Silvana1409@users.noreply.github.com>
2023-02-01 11:44:48 +0100

    Avancement exercice 23 jusqu'à min 3:09

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -5,10 +5,9 @@ Annéé : 2022-2023
 
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -28,10 +27,44 @@ class GameWonFragment : Fragment() {
             view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
         //obtenir les aguments du Groupe WON
-        var args = GameWonFragmentArgs.fromBundle(arguments!!)
+        //var args = GameWonFragmentArgs.fromBundle(arguments!!)
         //Pour afficher q les arguments ont été passés avec succès
-        Toast.makeText( context,"Num correct: ${args.numCorrect},NumQuestions: ${args.numQuestions}",
-            Toast.LENGTH_SHORT).show()
+        //Toast.makeText( context,"Num correct: ${args.numCorrect},NumQuestions: ${args.numQuestions}",
+          //  Toast.LENGTH_SHORT).show()
+
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.winner_menu,menu)
+    }
+    //méthode pour l'intention partage
+    private fun getShareIntent(): Intent{
+        //on déplace les arguments de fragment de WON
+        var args = GameWonFragmentArgs.fromBundle(arguments!!)
+        //nvelle intention implicite de partage
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        //Type de données q on va partager
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT,
+                getString(R.string.share_success_text,args.numCorrect,args.numQuestions))
+
+        return shareIntent
+
+    }
+    //méthode supp pour réaliser le partage
+    private fun shareSucces(){
+        //démarre l'activité avec l'intention qui démarre notre nvll intention
+        startActivity(getShareIntent())
+
+    }
+    //quand on appui sur partage
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item!!.itemId){
+            R.id.share->shareSucces()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
